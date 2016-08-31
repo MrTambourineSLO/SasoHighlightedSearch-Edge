bfe6597a49deca93164529b87a1d60155f6f47f3
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-08-31 20:39:21 +0200

    Added proper chome notification on change of primary search engine.

--- options.js
+++ options.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
     $("#sortable").disableSelection();
 
     $("#submitSearchEngine").click(function(){
+        //Store id of first list element when submit button is clicked
         var selectedSearchEngine = $("li:first").attr('id');
-        alert(selectedSearchEngine);
+        //Display notifications which search engine is primary 
+        var message = {
+            type: "basic",
+            title : "Primary search engine set.",
+            //Autocapitalize first letter of the selected search engine
+            message: "You search engine has been set to "+selectedSearchEngine.substr(0,1).toUpperCase()+selectedSearchEngine.substr(1),
+            iconUrl: "icon.png"
+        }
+        chrome.notifications.create('changed',message,function(){});
     })
     
 });
