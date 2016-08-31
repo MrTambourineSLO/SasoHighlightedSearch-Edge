b4c873a40ba3b8210cbf99071149ae1684af8a1f
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-08-31 20:26:21 +0200

    Functionality to store id of first element (ie primary search engine) into chrome storage.

--- options.js
+++ options.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $("#sortable").sortable();
     $("#sortable").disableSelection();
+
+    $("#submitSearchEngine").click(function(){
+        var selectedSearchEngine = $("li:first").attr('id');
+        alert(selectedSearchEngine);
+    })
+    
 });
