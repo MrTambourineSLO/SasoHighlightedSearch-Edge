3e413a8a0c9565f481850598d6fc61de1f454c74
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-09-03 00:07:38 +0200

    Added feature that updates context menu text according to changes in options + it sets correct value at startup.

--- eventPage.js
+++ eventPage.js
@@ -1,23 +1,40 @@
 //Create new menu item and register on click
-var selectedEngine = "";
-chrome.storage.sync.get('primary',function(items){
-    selectedEngine = items.primary;
-});
-
 var menuItem = {
-    "id" : "searchContext",
-    "title" : "Search with " + selectedEngine.toString(),
+    "id": "searchContext",
+    "title": "search with ",
     //Appear only when text is selected
-    "contexts" : ['selection']
+    "contexts": ['selection']
 }
-//Create context item
-chrome.contextMenus.create(menuItem);
-//Functionality of context item:
-chrome.contextMenus.onClicked.addListener(function(clickData){
-    //Was our search item clicked in context menu and is any text selected?
-    if(clickData.menuItemId == "searchContext" && clickData.selectionText){
-        //Compose url depending on search engine and query string
-        var searchUrl = selectedEngine + ".com/" + clickData.selectionText;
-        alert(searchUrl);
-    }
+
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.storage.sync.get('primary', function (items) {
+        alert(items.primary);
+        menuItem['title'] += items.primary;
+        alert(menuItem.title);
+        /*Event Listener for options change*/
+        //Create context item
+        chrome.contextMenus.create(menuItem);
+        /*END OF Event Listener for options change*/
+    });
+}, true);
+//If there was change of primary search engine in options, update contextMenu according to
+//message recieved from options page.
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    console.log(message.id+" recieved in event script");
+    chrome.contextMenus.update(menuItem.id,{title:'Search with '+message.id});
 });
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    //Was our search item clicked in context menu and is there any text selected?
+    if (clickData.menuItemId == "searchContext" && clickData.selectionText) {
+        //Compose url depending on search engine and query string
+        var searchUrl;
+        chrome.storage.sync.get('primary', function (items) {
+            if (items.primary) {
+                searchUrl = items.primary + ".com/" + clickData.selectionText;
+                alert(searchUrl);
+            }
+
+        });
+
+    }
+});  
