a0c2434680730d0142acb37c1d67db6965e41c56
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-09-29 10:15:29 +0200

    Cleared debug messages from options script.

--- options.js
+++ options.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     
     chrome.storage.sync.get("primary",function(items){
-    alert("Search engine form memory: "+items.primary);
+    
     //TODO: Set default checked radio button to google if there's no value in storage for 'primary'...
     if(items.primary == null){
-        alert("Setting it to google right now")
+        
         $("input[value='google']").prop('checked',true);
         
     }
@@ -19,7 +19,7 @@ $(document).ready(function () {
    $("#optionsForm").submit(function(){
        //Store value of selected checkbox
        var selected = $("input[name=searchEngine]:checked").val();
-      alert("SELECTED = "+selected);
+     
       //Save value to storage
       
       chrome.storage.sync.set({
